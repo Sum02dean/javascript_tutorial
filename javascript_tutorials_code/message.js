173e25d2a0b16237b284a855e828d8ea30cc0521
Dean Sumner <deans@MLS-M-DESU.local>
2022-01-03 19:36:37 +0100

    initial commit

---
+++ javascript_tutorials_code/message.js
@@ -0,0 +1,19 @@
+// All functions can be stored externally from html script.
+
+
+function msg() {
+    // commenting in is done as such.
+    alert("Hello Javapoit");
+    /* This is a muliline comment...
+    This will not be displayed */
+}
+
+function a() {
+    data = 200;
+    document.writeln(data);
+}
+
+function b() {
+    data = 200;
+    document.writeln(data);
+}
